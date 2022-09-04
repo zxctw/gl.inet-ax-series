34e23ff509540796da0959a28387e101d853dd76
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-09-04 20:41:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -43,7 +43,11 @@ fi
 
 
 cd wlan-ap/openwrt
-./scripts/gen_config.py $profile glinet_depends custom
+if [[ $ui == true  ]]; then 
+	./scripts/gen_config.py $profile glinet_depends custom
+else
+	./scripts/gen_config.py $profile openwrt_common luci custom
+fi
 
 git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
 ./scripts/feeds update -a 
